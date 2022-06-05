4cb5f2b5ab49e05535305b5cff9b8332bddeb9d1
DylanParenzee <dylanparenzee@hotmail.com>
2022-06-05 18:42:55 +1000

    Halfway

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(distInBlocks){
+    if(distInBlocks > 42){
+        return distInBlocks - 42
+    }
+    else if(distInBlocks < 42){
+        return 42 - distInBlocks;
+    }
+    return distInBlocks;
+}
+
+const distanceFromHqInFeet = (distInFeet) => {
+    return distanceFromHqInBlocks(distInFeet) * 264
+
+}
